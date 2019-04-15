a6c543fff4139fa08ca7c3409b1ac00547852fe2
Issei <gabriel_ozloco@hotmail.com>
2019-04-15 16:57:42 -0300

    Add files via upload

---
+++ exercicio-notas.py
@@ -0,0 +1,14 @@
+nota1=float(input("Insira a nota 1: "))
+nota2=float(input("Insira a nota 2: "))
+nota3=float(input("Insira a nota 3: "))
+nota4=float(input("Insira a nota 4: "))
+
+soma1=nota1+nota2
+soma2=nota3+nota4
+
+if soma1!="" and soma2!="":
+    result=soma1/2
+    result1=soma2/2
+    print("Resultado 1: ", result, "Resultado 2: ", result1)
+
+    
